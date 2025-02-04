dd21dd4ea1e59a11d9f746d7cce727830403512e
GrupoApiLexco <luis.chaparrochia@gmail.com>
2025-02-04 09:20:45 -0500

    Comentarios

--- app/Exceptions/InactiveUserException.php
+++ app/Exceptions/InactiveUserException.php
@@ -8,6 +8,6 @@ class InactiveUserException extends Exception {
     // Mensaje de error predeterminado cuando se lanza esta excepción
     protected $message = 'Usuario inactivo';
 
-    // Código de error HTTP asociado a esta excepción (403 - Prohibido)
-    protected $code = 403;
+    // Código de error HTTP asociado a esta excepción (401 error de autentificación.)
+    protected $code = 401;
 }
--- app/Exceptions/InvalidCredentialsException.php
+++ app/Exceptions/InvalidCredentialsException.php
@@ -8,6 +8,6 @@ class InvalidCredentialsException extends Exception {
     // Mensaje de error predeterminado cuando se lanza esta excepción
     protected $message = 'Credenciales inválidas';
 
-    // Código de error HTTP asociado a esta excepción (401 - No autorizado)
-    protected $code = 401;
+    // Código de error HTTP asociado a esta excepción (422 para errores de validación )
+    protected $code = 422;
 }
