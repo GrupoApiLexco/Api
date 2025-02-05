d8f7fce20c62f07ee22b8963db003e8100fde9b5
GrupoApiLexco <luis.chaparrochia@gmail.com>
2025-02-05 16:40:14 -0500

    Reglas de Laravel en el validador

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -8,7 +8,7 @@ class UserValidator
 {
     /**
      * Valida los datos de inicio de sesión.
-    */
+     */
     public static function validateLogin(array $data)
     {
         return Validator::make($data, [
@@ -25,27 +25,35 @@ class UserValidator
      */
     public static function validateCreate(array $data)
     {
+        // Verifica si la validación es precognitiva
+        $isPrecognitive = isset($data['is_precognitive']) && $data['is_precognitive'];
+
         return Validator::make($data, [
             'name' => 'required|string|max:255', // Nombre obligatorio, debe ser un string y máximo de 255 caracteres.
             'surnames' => 'required|string|max:255', // Apellidos obligatorios, con las mismas restricciones.
             'email' => 'required|email|unique:users', // El email debe ser único en la tabla de usuarios.
-            'password' => 'required|string|min:8', // La contraseña es obligatoria y debe tener al menos 8 caracteres.
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                // Si es precognitiva, solo valida que tenga al menos 8 caracteres, de lo contrario valida que no esté comprometida
+                $isPrecognitive ? 'nullable' : 'uncompromised', // Si es precognitiva, la contraseña solo necesita tener 8 caracteres y no es necesario validarla como "no comprometida"
+            ],
             'role' => 'required|in:admin,vendedor,cliente' // El rol es obligatorio y debe ser uno de los valores permitidos.
         ]);
     }
 
-
     public static function validateToggleStatus(array $data)
-{
-    return Validator::make($data, [
-        'user_id' => 'required|exists:users,id'
-    ]);
-}
+    {
+        return Validator::make($data, [
+            'user_id' => 'required|exists:users,id'
+        ]);
+    }
 
     /**
      * Valida los datos para la actualización de un usuario.
      *
-    */
+     */
     public static function validateUpdate(array $data): \Illuminate\Validation\Validator
     {
         return Validator::make($data, [
@@ -59,7 +67,7 @@ class UserValidator
     /**
      * Valida la imagen subida por el usuario.
      *
-    */
+     */
     public static function validateImage(array $data): \Illuminate\Validation\Validator
     {
         return Validator::make($data, [
